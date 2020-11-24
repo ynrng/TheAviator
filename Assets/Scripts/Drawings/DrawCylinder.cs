38aa16f6ebb83e0439e82cc1e7e5e2e7d8ab25a1
suga <suga_e@outlook.com>
2020-11-25 05:10:00 +0800

    align vertices calculation

--- Assets/Scripts/Drawings/DrawCylinder.cs
+++ Assets/Scripts/Drawings/DrawCylinder.cs
@@ -7,7 +7,7 @@ public class DrawCylinder : MonoBehaviour {
     // how many segments do we want in between;
     public int segmentsHeight = 1; //
     public int segmentsRadial = 3;
-    public bool isDiscrete = false;
+    public bool isDiscrete = true;
     int radiusRound = 1;
     List<Vector3> vertices;
     List<int> triangles;
@@ -27,29 +27,20 @@ public class DrawCylinder : MonoBehaviour {
         for (int j = 0; j <= segmentsHeight; j++) {
             for (int i = 0; i < segmentsRadial; i++) {
                 int indexk = j * segmentsRadial + i;
-                bool isZero = i % segmentsRadial == 0;
+                int zeroBalance = i % segmentsRadial == 0 ? segmentsRadial : 0;
                 points[indexk] = new List<int>();
                 // mind edge case
                 if (j != segmentsHeight) {
                     // top line
-                    if (isZero) {
-                        points[indexk].Add((indexk + segmentsRadial) * 6 - 4); // t l
-                    } else {
-                        points[indexk].Add((indexk) * 6 - 4); // t l
-                    }
+                    points[indexk].Add((indexk + zeroBalance) * 6 - 3); // t r t
+                    points[indexk].Add((indexk + zeroBalance) * 6 - 4); // t r t
                     points[indexk].Add((indexk) * 6); // t r b
-                    points[indexk].Add((indexk) * 6 + 3); // t r t
                 }
                 if (j != 0) {
                     // bottome line
-                    if (isZero) {
-                        points[indexk].Add(indexk * 6 - 5); // b l b
-                        points[indexk].Add(indexk * 6 - 1); // b l t
-                    } else {
-                        points[indexk].Add((indexk - segmentsRadial) * 6 - 5); // b l b
-                        points[indexk].Add((indexk - segmentsRadial) * 6 - 1); // b l t
-                    }
+                    points[indexk].Add((indexk - segmentsRadial) * 6 + 1); // b r
                     points[indexk].Add((indexk - segmentsRadial) * 6 + 4); // b r
+                    points[indexk].Add((indexk - segmentsRadial + zeroBalance) * 6 - 1); // b r
                 }
             }
         }
@@ -73,39 +64,30 @@ public class DrawCylinder : MonoBehaviour {
         //add back origin point so in for loop it dose not overflow
         xzs.Add(new Vector3(Mathf.Cos(0), 0, Mathf.Sin(0)) * radiusRound);
 
+        //
+        //start generation
+        vertices = new List<Vector3>();
+        //faces: basicly from 0 to segmentsHeight*segmentsRound-1
+        triangles = new List<int>();
+
         if (isDiscrete) {
-            //
-            //start generation
-            vertices = new List<Vector3>();
             //faces: basicly from 0 to segmentsHeight*segmentsRound-1
             triangles = Enumerable.Range(0, (segmentsHeight * segmentsRadial) * 6).ToList();
 
             // only need to generate side
             for (int j = 0; j < segmentsHeight; j++) {
                 for (int i = 0; i < segmentsRadial; i++) {
-                    //order matters
-                    //points
-                    vertices.Add(xzs[i] + Vector3.up * (j + tranferToCenterY));
-                    vertices.Add(xzs[i + 1] + Vector3.up * (j + 1 + tranferToCenterY));
-                    vertices.Add(xzs[i + 1] + Vector3.up * (j + tranferToCenterY));
-
+                    //order matters. points
                     vertices.Add(xzs[i] + Vector3.up * (j + tranferToCenterY));
                     vertices.Add(xzs[i] + Vector3.up * (j + 1 + tranferToCenterY));
+                    vertices.Add(xzs[i + 1] + Vector3.up * (j + tranferToCenterY));
+
+                    vertices.Add(xzs[i + 1] + Vector3.up * (j + tranferToCenterY));
+                    vertices.Add(xzs[i] + Vector3.up * (j + 1 + tranferToCenterY));
                     vertices.Add(xzs[i + 1] + Vector3.up * (j + 1 + tranferToCenterY));
-
-                    // int start = segmentsRound * j + i;
-                    // triangles.AddRange(Enumerable.Range(start * 3, 3));
                 }
-
             }
         } else {
-            //
-            //start generation
-            vertices = new List<Vector3>();
-            // vertices.AddRange(xzs);
-            //faces: basicly from 0 to segmentsHeight*segmentsRound-1
-            triangles = new List<int>();
-
             // only need to generate side
             for (int j = 0; j < segmentsHeight + 1; j++) {
                 for (int i = 0; i < segmentsRadial; i++) {
