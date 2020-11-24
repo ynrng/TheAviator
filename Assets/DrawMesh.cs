a66b53c0f2f8dd65fa4bbc7e7291e1a959df174f
suga <suga_e@outlook.com>
2020-11-25 07:13:00 +0800

    draw a triangle

---
+++ Assets/DrawMesh.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class DrawMesh : MonoBehaviour {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMeshData();
+        Draw();
+    }
+
+    void CreateMeshData()
+    {
+        vertices = new Vector3[]{
+            new Vector3(0, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 0, 0),
+        };
+        triangles = new int[]{
+            0, 1, 2
+        };
+    }
+
+    void Draw()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+}
